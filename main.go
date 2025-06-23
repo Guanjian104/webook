fb8b7cdefebe5a7d9e31b532feffcd73e928cab1
Guanjian104 <gl451845102@163.com>
2025-06-24 02:26:59 +0800

    改造代码，去除对Mysql和Redis的依赖

--- main.go
+++ main.go
@@ -14,17 +14,22 @@ import (
 	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"net/http"
 	"strings"
 	"time"
 )
 
 func main() {
-	db := initDB()
+	// db := initDB()
 
-	server := initWebServer()
+	// server := initWebServer()
 
-	initUserHdl(db, server)
+	// initUserHdl(db, server)
 
+	server := gin.Default()
+	server.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello world")
+	})
 	server.Run(":8080")
 }
 
